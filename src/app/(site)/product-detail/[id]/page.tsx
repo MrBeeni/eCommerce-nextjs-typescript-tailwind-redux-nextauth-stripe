a896ea2f0d3b99326f3ed483be8a8f875ab88bdc
MrBeeni <mubeen4429@gmail.com>
2023-12-03 16:36:45 +0500

    checking issues

--- src/app/(site)/product-detail/[id]/page.tsx
+++ src/app/(site)/product-detail/[id]/page.tsx
@@ -1,20 +1,32 @@
+"use client";
 import Container from "@/components/Container";
 import ProductDetails from "@/components/ProductDetails";
 import { getProductsById } from "@/helpers";
+import { useEffect, useState } from "react";
 
 type Props = {
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
-const Page = async ({ searchParams }: Props) => {
+const Page = ({ searchParams }: Props) => {
+  const [product, setProduct] = useState<any>();
   const idString = searchParams?.id;
   console.log(searchParams);
   const id = Number(idString);
-  const product = await getProductsById(id);
+
+  useEffect(() => {
+    const loadData = async () => {
+      const product = await getProductsById(id);
+      setProduct(product);
+    };
+
+    loadData();
+  }, [id]);
 
   return (
     <Container className="flex items-center flex-col md:flex-row px-4 xl:px-0">
-      <ProductDetails product={product} />
+      <div>loading {id} </div>
+      {/* <ProductDetails product={product} /> */}
     </Container>
   );
 };
